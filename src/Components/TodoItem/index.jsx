c5eb9b1fb0f1b7658e87dc878c05a6344df8aee8
Fernando Perez <fernandohps17@gmail.com>
2024-10-22 22:03:52 -0400

    Se crearon los eventos para completar y eliminar todos

--- src/Components/TodoItem/index.jsx
+++ src/Components/TodoItem/index.jsx
@@ -3,13 +3,13 @@ import { IoCloseSharp } from "react-icons/io5";
 import React from 'react'
 import './TodoItem.scss'
 
-const TodoItem = ({text, completed}) => {
+const TodoItem = ({text, completed, onComplete, onDelete}) => {
     return (
         <>
             <li className='todo_item'>
-                <span className={`todo_item_check ${completed && 'todo_item_check_completed'}`}><FaCheck /></span>
+                <span className={`todo_item_check ${completed && 'todo_item_check_completed'}`} onClick={onComplete}><FaCheck /></span>
                 <p className={`todo_item_text ${completed && 'todo_item_text_completed'}`}>{text}</p>
-                <span className='todo_item_close'><IoCloseSharp /></span>
+                <span className='todo_item_close' onClick={onDelete}><IoCloseSharp /></span>
             </li>
         </>
     )
